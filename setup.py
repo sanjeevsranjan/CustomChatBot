ccde6d6cf7362de24d269f99a095c2441e44386e
Sanjeev Ranjan <sanjeevr1101@gmail.com>
2026-07-08 00:22:56 +0530

    Basic Setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= "custom-chatbot",
+    version= "0.1.0",
+    author= "Sanjeev Ranjan",
+    author_email="sanjeevr1101@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
